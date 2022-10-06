17d69de88e51b62748cc633e2c08f079b1255c8b
Manuel <imanuel96i@gmail.com>
2022-10-06 00:29:51 -0300

    Update files

--- bot.py
+++ bot.py
@@ -9,15 +9,20 @@ load_dotenv(override=True)
 TOKEN = os.getenv('TOKEN')
 TOKENCHKR = int(os.getenv('TOKENCHKR'))
 RSS = os.getenv('RSS')
-RSS2 = os.getenv('RSS2')
+# RSS2 = os.getenv('RSS2')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    feedRSSKR.start(TOKENCHKR,RSS2,client)
+    feedRSSKR.start(TOKENCHKR,RSS,client)
 
+@client.event
+async def on_message(message):
+    if message.channel.id == (825529446345736214):
+        await client.get_channel(711454318372323399).send(f'{message.content}')
+        print(f'{message.author.name}: {message.content}')
 
 client.run(TOKEN)
 
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+cmd /k "cd /d D:\BOTS\discordrsscodeclosers\env\Scripts & activate & cd /d D:\BOTS\discordrsscodeclosers & py bot.py"
+pause
